3ff46cdaef3468f397389b4dbfd7d0073abbec6e
Brayden Blank <99354754+Brayden-Blank@users.noreply.github.com>
2022-06-12 19:40:54 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// Brayden Blank
+// ICS2O-Unit-6-02-HTML
+// May 2022
+
+'use strict'
+/**
+ * Check service worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-6-02-HTML/sw.js", {
+    scope: "/ICS2O-Unit-6-02-HTML/",
+  })
+}
+
+/**
+ * Updates the click counter.
+ */
+function updateCount() {
+  if (localStorage.clicks) {
+    document.getElementById("times-clicked-text").innerHTML = "<h5>Times Clicked: " + localStorage.clicks + "</h5>"
+  } else {
+    localStorage.clicks = 0
+  }
+}
+/**
+ * Resets the click counter to 0.
+ */
+function resetProgress() {
+  localStorage.clicks = 0
+  updateCount()
+}
+
+/**
+ * Adds one to the click counter.
+ */
+function onClick() {
+  localStorage.clicks++
+  updateCount()
+}
